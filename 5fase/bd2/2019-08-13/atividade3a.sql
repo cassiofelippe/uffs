432734d3aa027da04c0e2edcdde0c5d1ef911349
Cassio <cassioluis12@gmail.com>
2019-08-15 19:21:16 -0300

    Adicionada atividade de banco de dados 2

---
+++ 5fase/bd2/2019-08-13/atividade3a.sql
@@ -0,0 +1,69 @@
+CREATE TABLE page_revision (
+	id int,
+	name varchar(50),
+	created_on timestamp,
+	author varchar(50),
+	text text
+);
+
+CREATE TABLE page_audition (
+	id_page int,
+	name varchar(50),
+	created_on timestamp,
+	author varchar(50),
+	text text,
+	updated_on timestamp,
+	user_login varchar(20),
+	dif_length int,
+	text_edited TEXT,
+	operation varchar(6)
+);
+
+CREATE OR REPLACE FUNCTION audit_page() RETURNS TRIGGER AS $$
+    BEGIN
+	    RAISE NOTICE 'been here with %, %', OLD.id, old;
+        IF OLD.id IS NULL THEN -- is new record
+            INSERT INTO page_audition VALUES (
+            	NEW.id, NEW.name, NEW.created_on, NEW.author, NEW.TEXT, CURRENT_TIMESTAMP, (SELECT current_user),
+            	length(NEW.TEXT), NEW.TEXT, 'INSERT'
+            );
+		END IF;
+		
+		RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER audit_page ON page_revision;
+CREATE TRIGGER audit_page AFTER INSERT OR UPDATE ON page_revision FOR EACH ROW EXECUTE PROCEDURE audit_page();
+
+SELECT * FROM page_revision;
+SELECT * FROM page_audition;
+
+INSERT INTO page_revision VALUES (1001, 'Genesis - Band', current_timestamp, 'SomeFanOfTheBand68', 'Genesis were an English rock band formed at Charterhouse School, Godalming, Surrey, in 1967. The most successful and longest-lasting line-up consisted of keyboardist Tony Banks, bassist/guitarist Mike Rutherford and drummer/singer Phil Collins.');
+
+DELETE FROM page_revision;
+DELETE FROM page_audition;
+
+DROP TABLE page_audition;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
