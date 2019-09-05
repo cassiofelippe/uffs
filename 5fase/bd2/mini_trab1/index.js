5e8a876c6600107f91f7c73da3dd328d19f76830
Cassio Felippe <cassioluis12@gmail.com>
2019-09-05 17:30:41 -0300

    Atualizado

--- 5fase/bd2/mini_trab1/index.js
+++ 5fase/bd2/mini_trab1/index.js
@@ -3,18 +3,39 @@ var pg = require('pg');
 var client = new pg.Client("postgres://postgres:postgres@localhost:5432/mini_trab1");
 client.connect();
 
+(async () => {
+  // ESSA TRANSAÇÃO FUNCIONA
+  await client.query('BEGIN')
+  await client.query(`INSERT INTO test (name, created_on) VALUES('OK', current_timestamp) RETURNING id`);
+  await client.query('COMMIT')
+
+  /*
+    ESSA TRANSAÇÃO CAUSARÁ FALHA, POIS ESTÁ TENTANDO INSERIR UMA STRING NO LUGAR DE TIMESTAMP
+    LOGO SERÁ FEITO ROLLBACK AUTOMATICAMENTE
+  */
+  await client.query('BEGIN')
+  await client.query(`INSERT INTO test (name, created_on) VALUES('MUST NOT BE INSERTED', current_timestamp) RETURNING id`);
+  await client.query(`INSERT INTO test (name, created_on) VALUES('ERROR', '1762getsyt') RETURNING id`);
+  await client.query('COMMIT')
+})().catch(e => console.log('Houve falha na transação'))
+
+
+
+
 /*
-client.query("INSERT INTO test (name, created_on) values ('Cassio', current_timestamp)");
-client.query("INSERT INTO test (name, created_on) values ('Lucas', current_timestamp)");
-client.query("INSERT INTO test (name, created_on) values ('Vinicius', current_timestamp)");
+DATABASE
+
+CREATE DATABASE mini_trab1;
+
+CREATE TABLE public.test (
+    id integer NOT NULL,
+    name character varying(50),
+    created_on timestamp without time zone
+);
+
 */
 
-(async () => {
-  await client.query('BEGIN')
-  await client.query(`INSERT INTO test (name, created_on) VALUES('ERROR', current_timestamp) RETURNING id`);
-  await client.query(`INSERT INTO test (name, created_on) VALUES('ERROR', 1762getsyt) RETURNING id`);
-  await client.query('COMMIT')
-})().catch(e => console.error(e.stack))
+
 
 
 
