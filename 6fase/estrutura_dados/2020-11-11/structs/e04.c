529b0c25359d0776cc03f13a0e90307bfd543275
Cassio L Z F Felippe <cassioluis12@gmail.com>
2020-11-13 19:48:24 -0300

    finished e04 (all from structs)

---
+++ 6fase/estrutura_dados/2020-11-11/structs/e04.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int day;
+	int month;
+	int year;
+} Date;
+
+typedef struct {
+	int id;
+	char nome[41];
+	double salario;
+	Date *birthdate;
+} Employee;
+
+void print_emp_birthdate(Employee *emp);
+
+void compare_dates(Date *one, Date *two, int *result);
+
+int main() {
+	Date *birth = malloc(sizeof(Date));
+	Employee *emp = malloc(sizeof(Employee));
+	int *result = malloc(sizeof(int)), *aux = malloc(sizeof(int));
+
+	scanf("%d", &emp->birthdate->day);
+	scanf("%d", &emp->birthdate->month);
+	scanf("%d", &emp->birthdate->year);
+
+	print_emp_birthdate(emp);
+
+	free(birth);
+	free(emp);
+}
+
+void print_emp_birthdate(Employee *emp) {
+	printf(
+		"Employee birthdate %d/%d/%d\n",
+		emp->birthdate->day,
+		emp->birthdate->month,
+		emp->birthdate->year
+	);
+}
