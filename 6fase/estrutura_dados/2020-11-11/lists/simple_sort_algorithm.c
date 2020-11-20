40f672367397fcf014437d63bd14cff46460ce85
Cassio Felippe <cassioluis12@gmail.com>
2020-11-20 11:16:23 -0300

    Implemented simples sort algorithm using int[]

---
+++ 6fase/estrutura_dados/2020-11-11/lists/simple_sort_algorithm.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 4 // length
+
+
+/* DEF METHODS */
+
+void sort(int *a, int i);
+void print(int *a);
+
+/* MAIN */
+
+int main() {
+	int a[N] = {1, 21, 4, 6};
+
+	printf("\nbefore:");
+	print(a);
+
+	sort(a, 1);
+
+	printf("\nsorted:");
+	print(a);
+
+	printf("\n");
+
+	return 0;
+}
+
+/* param num must be the first of the array */
+void sort(int *a, int i) {
+	int aux, j = i - 1;
+
+	if (i == 0) {
+		return sort(a, i + 1);
+	} else if (i == N) {
+		return;
+	}
+
+	if (a[j] > a[i]) {
+		aux = a[i];
+		a[i] = a[j];
+		a[j] = aux;
+		return sort(a, 1);
+	}
+
+	return sort(a, i + 1);
+}
+
+void print(int *a) {
+	int i;
+	for (i = 0; i < N; i++) {
+		printf(" %d", a[i]);
+	}
+}
