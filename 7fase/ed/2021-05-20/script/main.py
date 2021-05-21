6104a7285c0f7a327a271e1d3e333c2f98e578d5
Cassio Felippe <cassioluis12@gmail.com>
2021-05-21 00:41:01 -0300

    developed python script to help insert fields

---
+++ 7fase/ed/2021-05-20/script/main.py
@@ -0,0 +1,38 @@
+import pyautogui as py
+import time
+
+# py.keyDown('ctrl')
+# py.press('pgup')
+# py.keyUp('ctrl')
+
+py.write('clear;clear;gcc main.c -o main -Wall && ./main\n')
+
+py.write('1\n')
+py.write('Cassio\n')
+py.write('15/05/1998\n')
+py.write('cassioluis12@gmail.com\n')
+py.write('49999076237\n')
+
+py.write('1\n')
+py.write('Lucas\n')
+py.write('08/01/1999\n')
+py.write('lucas.rei.delas@gmail.com\n')
+py.write('199999999\n')
+
+py.write('2\n')
+py.write('cassioluis12@gmail.com\n')
+py.write('John\n')
+py.write('20/05/2021\n')
+py.write('john@invented.net\n')
+py.write('2222\n')
+
+py.write('2\n')
+py.write('emailnotfound\n')
+py.write('John\n')
+py.write('20/05/2021\n')
+py.write('john@invented.net\n')
+py.write('2222\n')
+
+py.write('6\n')
+
+# py.write('10\n')
